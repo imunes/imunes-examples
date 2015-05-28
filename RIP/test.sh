d96776eb46608fd409c20b8cf3871360830e8f3f
Denis Salopek <denis.sale@gmail.com>
2015-05-28 12:38:06 +0200

    Added ripng example for IPv6 testing.

--- RIP/test.sh
+++ RIP/test.sh
@@ -9,39 +9,24 @@ eid=`imunes -b RIP1.imn | tail -1 | cut -d' ' -f4`
 startCheck "$eid"
 
 sleep 10
-pingCheck pc@$eid 10.0.4.10
+netDump router2@$eid eth2
 if [ $? -eq 0 ]; then
-    sleep 2
-    netDump router2@$eid eth2
-    echo ""
-    echo "########## router2@$eid routes"
-    himage router2@$eid vtysh << __END__
-    show ip rip
-    exit
+    pingCheck pc@$eid 10.0.4.10
+    if [ $? -eq 0 ]; then
+	echo "########## router2@$eid routes"
+	himage router2@$eid vtysh << __END__
+	show ip rip
+	show ipv6 ripng
+	exit
 __END__
-    if [ $? -ne 0 ]; then
-	err=1
-    elif [ $slow -eq 1 ]; then
-	stopNode router7@$eid 
+	sleep 2
 	if [ $? -eq 0 ]; then
-	    sleep 190
-
-	    echo ""
-	    echo "########## router2@$eid routes after 3 minutes"
-	    himage router2@$eid vtysh << __END__ 
-	    show ip rip
-	    exit
-__END__
-
-	    startNode router7@$eid
+	    ping6Check pc@$eid fc00:1::10
 	    if [ $? -eq 0 ]; then
-		sleep 10
-
-		pingCheck pc@$eid 10.0.4.10
-		if [ $? -eq 0 ]; then
-		    sleep 4
-		    readDump router2@$eid eth2
-		else
+		sleep 2
+		echo ""
+		readDump router2@$eid eth2
+		if [ $? -ne 0 ]; then
 		    err=1
 		fi
 	    else
@@ -50,6 +35,46 @@ __END__
 	else
 	    err=1
 	fi
+
+	if [ $slow -eq 1 ]; then
+	    stopNode router7@$eid 
+	    if [ $? -eq 0 ]; then
+		sleep 190
+
+		echo ""
+		echo "########## router2@$eid routes after 3 minutes"
+		himage router2@$eid vtysh << __END__ 
+		show ip rip
+		exit
+__END__
+
+		startNode router7@$eid
+		if [ $? -eq 0 ]; then
+		    sleep 10
+
+		    pingCheck pc@$eid 10.0.4.10
+		    if [ $? -eq 0 ]; then
+			sleep 4
+			readDump router2@$eid eth2
+		    else
+			err=1
+		    fi
+		    ping6Check pc@$eid fc00:1::10
+		    if [ $? -eq 0 ]; then
+			sleep 4
+			readDump router2@$eid eth2
+		    else
+			err=1
+		    fi
+		else
+		    err=1
+		fi
+	    else
+		err=1
+	    fi
+	fi
+    else
+	err=1
     fi
 else
     err=1
