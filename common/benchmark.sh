d4144e017b2882296a96e14ac9f2b4674906a45a
oikuda <valter.vasic@gmail.com>
2015-07-15 14:29:06 +0200

    updated benchmark script
    Better info output, added possibility to remove sleep between actions. Better
    argument parsing.

--- common/benchmark.sh
+++ common/benchmark.sh
@@ -36,36 +36,66 @@ termTopo() {
     egrep -o "[0-9]+\.[0-9]+ " term_$1.log
 }
 
+printDockerInfo() {
+    srvver=`docker version | grep "Server version:"| cut -d: -f2`
+    apiver=`docker version | grep "Server API version:"| cut -d: -f2`
+    gitver=`docker version | grep "Git commit (server):"| cut -d: -f2`
+    echo "Docker version$srvver (API:$apiver, git:$gitver)"
+    docker info 2> /dev/null | grep -A 2 Storage | sed -e 's/^ */\t/'
+}
+
+printLinuxHardwareInfo() {
+    echo "Hardware info:"
+    echo "CPU:`cat /proc/cpuinfo | grep "model name" | head -1 | cut -d: -f2`" | sed -e 's/^/\t/'
+    echo "Cores: `cat /proc/cpuinfo | grep processor | wc -l`" | sed -e 's/^/\t/'
+    mem_gb=$(echo "scale=2; `cat /proc/meminfo | grep MemTotal | awk '{print $2}'`/1024/1024" | bc -l)
+    echo "RAM: $mem_gb GB" | sed -e 's/^/\t/'
+}
+
+printFreebsdHardwareInfo() {
+    echo "Hardware info:"
+    echo "CPU:`sysctl hw.model | cut -d: -f2`" | sed -e 's/^/\t/'
+    echo "Cores:`sysctl hw.ncpu | cut -d: -f2`" | sed -e 's/^/\t/'
+    mem_gb=$(echo "scale=2; `sysctl hw.realmem | cut -d: -f2`/1024/1024/1024" | bc -l)
+    echo "RAM: $mem_gb GB" | sed -e 's/^/\t/'
+}
+
 #argument parsing
 os=`uname -s`
 count=10
 wait_before_termination=10
+do_sleep=1;
 
-if test "$1" = "-c"; then
-    count=$2
-    shift 2
-fi
+while getopts ":c:i" opt; do
+    case $opt in
+	c) count=$OPTARG;;
+	i) do_sleep=0;;
+	\?) echo "Invalid option: -$OPTARG" >&2;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 tests="$*"
 
 # print system info
+uname -srvm
 if test "$os" = "FreeBSD"; then
     imunes -i
-    uname -a
-    echo "CPU:`sysctl hw.model | cut -d: -f2`"
-    echo "Cores:`sysctl hw.ncpu | cut -d: -f2`"
-    mem_gb=$(echo "scale=2; `sysctl hw.realmem | cut -d: -f2`/1024/1024/1024" | bc -l)
+    printFreebsdHardwareInfo
 elif test "$os" = "Linux"; then
-    uname -a
-    echo "CPU:`cat /proc/cpuinfo | grep "model name" | head -1 | cut -d: -f2`"
-    echo "Cores: `cat /proc/cpuinfo | grep processor | wc -l`"
-    mem_gb=$(echo "scale=2; `cat /proc/meminfo | grep MemTotal | awk '{print $2}'`/1024/1024" | bc -l)
+    printDockerInfo 
+    printLinuxHardwareInfo
 else
     echo "OS not supported."
     exit 1
 fi
 
-echo "RAM: $mem_gb GB"
+if test "$tests" = ""; then
+    echo "No topologies specified... exiting."
+    exit 0
+fi
+
 echo "Benchmarking topologies: $tests"
 echo "Number of iterations: $count"
 
@@ -83,19 +113,27 @@ for t in $tests; do
 	# start topology
 	start_time=`startTopo $eid $t`
 	echo -n "start: $start_time "
-	# wait for topology stabilization
-	sleep $start_time
-	# wait before termination
-	sleep $wait_before_termination
+	if test $do_sleep -eq 1; then
+	    # wait for topology stabilization
+	    sleep $start_time
+	    # wait before termination
+	    sleep $wait_before_termination
+	fi
 	# calculate memusage
 	freem_after=`mfree`
-	memusage=$(($freem_before-$freem_after))
-	echo -n "memusage: $memusage "
+	if test $i -eq 1; then
+	    memusage=0
+	else
+	    memusage=$(($freem_before-$freem_after))
+	    echo -n "memusage: $memusage "
+	fi
 	# terminate topology
 	term_time=`termTopo $eid`
 	echo "term: $term_time "
-	# wait for system stabilization
-	sleep $term_time
+	if test $do_sleep -eq 1; then
+	    # wait for system stabilization
+	    sleep $term_time
+	fi
 	freem_end=`mfree`
 	# accumulate values for average
 	sum_start=`echo $sum_start+$start_time | bc -l`
