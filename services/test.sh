4440e23da0af455e5bc1e86523e628e082b9abe7
denisSal <denis.sale@gmail.com>
2015-10-29 13:27:38 +0000

    added example for services in IMUNES
    Services are selected in the Configure window of host nodes.

---
+++ services/test.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. ../common/procedures.sh
+
+err=0
+
+eid=`imunes -b services.imn | awk '/Experiment/{print $4; exit}'`
+startCheck "$eid"
+
+# ftp
+himage FTP netstat -an | grep LISTEN | grep -q ":21 "
+if [ $? -ne 0 ]; then
+    echo "FTP error"
+    err=1
+fi
+
+# ssh
+himage SSH netstat -an | grep LISTEN | grep -q ":22 "
+if [ $? -ne 0 ]; then
+    echo "SSH error"
+    err=1
+fi
+
+# telnet
+himage TELNET netstat -an | grep LISTEN | grep -q ":23 "
+if [ $? -ne 0 ]; then
+    echo "TELNET error"
+    err=1
+fi
+
+imunes -b -e $eid
+
+# tcpdump
+# testing after termination because that's when the file is saved in /tmp/$eid
+file /tmp/$eid/TCPDUMP_n3_eth0.pcap | grep -q "tcpdump capture file"
+if [ $? -ne 0 ]; then
+    echo "TCPDUMP error"
+    err=1
+fi
+
+thereWereErrors $err
