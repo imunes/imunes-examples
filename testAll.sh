e67dd94026da5e98ec55536e7b40a2a54b4a2c03
denisSal <denis.sale@gmail.com>
2015-10-30 13:51:28 +0000

    added sequential option for testAll script
    This is to prevent failures that happen on systems with low processing power (e.g. Linux or constrained FreeBSD systems).
    Separated Linux and FreeBSD tests list.

--- testAll.sh
+++ testAll.sh
@@ -6,49 +6,69 @@ if [ "`whoami`" != "root" ]; then
     exit 1
 fi
 
+sequential=0
+if test "$1" = "seq"; then
+    sequential=1
+fi
+
 imunes -i
 
-tests="DHCP DNS+Mail+WEB OSPF Ping RIP Traceroute functional_tests/rj45_vlan"
+tests="DHCP DNS+Mail+WEB OSPF Ping RIP Traceroute services"
+
+if test `uname -s` = "FreeBSD"; then
+    tests="$tests functional_tests/rj44_vlan gif"
+fi
 
 echo "#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*"
-echo "# Starting background tests in:"
+echo "# Running tests in:"
 echo -n "# "
 for dir in $tests; do
     echo -n "$dir "
     cd $dir
-    if [ "$dir" == "ipsec/" ]; then
+    if test "$dir" = "ipsec/"; then
 	for i in 44 46 64 66; do
-	    sh "test${i}.sh" > TESTRESULTS_$i 2>&1 &
-	    pids="$pids $!"
+	    if [ $sequential -eq 1 ]; then
+		sh "test${i}.sh" > TESTRESULTS_$i 2>&1
+	    else
+		sh "test${i}.sh" > TESTRESULTS_$i 2>&1 &
+		pids="$pids $!"
+	    fi
 	done
     else
-	sh test.sh > TESTRESULTS 2>&1 &
-	pids="$pids $!"
+	if [ $sequential -eq 1 ]; then
+	    sh test.sh > TESTRESULTS 2>&1
+	else
+	    sh test.sh > TESTRESULTS 2>&1 &
+	    pids="$pids $!"
+	fi
     fi
     cd ..
 done
 
-running=1;
-echo ""
-echo "Some tests are still running in the background."
-echo "Please wait until everything has finished."
-echo ""
-echo -n "Running ."
-while test $running -eq 1
-do
-    running=0
-    for p in $pids
+if test ! -z "$pids" ; then
+    running=1;
+    echo ""
+    echo "Some tests are still running in the background."
+    echo "Please wait until everything has finished."
+    echo ""
+    echo -n "Running ."
+    while test $running -eq 1
     do
-        ps $p > /dev/null 2>&1
-        if test $? -eq 0; then
-            running=1
-            sleep 3
-            echo -n .
-            break
-        fi
+	running=0
+	for p in $pids
+	do
+	    ps $p > /dev/null 2>&1
+	    if test $? -eq 0; then
+		running=1
+		sleep 3
+		echo -n .
+		break
+	    fi
+	done
     done
-done
-echo ""
+    echo ""
+fi
+
 echo "Finished."
 
 grep "^There were errors." */TESTRESULTS* > /dev/null 2>&1
