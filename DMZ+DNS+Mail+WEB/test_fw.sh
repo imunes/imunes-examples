69804ea43d93b1371c681443eee671669badb74e
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2015-11-23 13:32:36 +0100

    Test firewall using IP addresses, not DNS names

--- DMZ+DNS+Mail+WEB/test_fw.sh
+++ DMZ+DNS+Mail+WEB/test_fw.sh
@@ -25,30 +25,35 @@ himage pc nmap -Pn -p20-25,53,80 15.16.17.2
 echo ""
 echo "Scan wwwMM.mm.com from outside network"
 echo "Open: http"
-himage pc nmap -Pn -p20-25,53,80 wwwMM.mm.com
+himage pc nmap -Pn -p20-25,53,80 15.16.17.80
 
 echo ""
 echo "Scan dnsMM.mm.com from outside network"
-echo "Open: domain"
-himage pc nmap -Pn -p20-25,53,80 dns.mm.com
+echo "Domain is closed. Access is allowed only from secondary server dnsTel"
+himage pc nmap -Pn -p20-25,53,80 15.16.17.18
+
+echo ""
+echo "Scan dnsMM.mm.com from dnsTel"
+echo "Open: domain (only from dnsTel: secundary server)"
+himage dnsTel nmap -Pn -p20-25,53,80 15.16.17.18
 
 echo ""
 echo "Scan smtpMM.mm.com from outside network"
 echo "Open: smtp"
-himage pc nmap -Pn -p20-25,53,80 smtpMM.mm.com
+himage pc nmap -Pn -p20-25,53,80 15.16.17.25
 
 echo ""
 echo "Scan www.tel.fer.hr from DMZ"
 echo "Open: smtp (http is not allowed from DMZ)"
-himage smtpMM nmap -Pn -p20-25,53,80 www.tel.fer.hr
+himage smtpMM nmap -Pn -p20-25,53,80 20.0.0.3
 
 echo ""
 echo "Scan www.tel.fer.hr from LAN"
 echo "Open: smtp,http"
-himage pc1 nmap -Pn -p20-25,53,80 www.tel.fer.hr
+himage pc1 nmap -Pn -p20-25,53,80 20.0.0.3
 
 echo ""
 echo "Scan UDP prots on dnsMM.mm.com from outside network"
 echo "Open: domain (63 open|filtered)"
-himage pc nmap -Pn -p7-70 -sU dns.mm.com
+himage pc nmap -Pn -p7-70 -sU 15.16.17.18
 
