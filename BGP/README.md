337ee59e6094e366d24e3f7aa88524dde7d71eed
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2017-07-05 12:01:42 +0200

    BGP examples; initial upload

---
+++ BGP/README.md
@@ -0,0 +1,25 @@
+# BGP
+BGP routing protocol examples
+
+#### BGP_custom-config.imn
+In this example "Custom Config" feature is used for BGP router's configuration.
+
+Double click on router or select "Configure" from right-click popup menu:
+-- Custom startup config is "Enabled"
+-- Selected custom config is "conf1"
+-- Click on "Editor" button
+
+(configuration is taken from: "Configuring and Testing Border Gateway
+Protocol (BGP) on Basis of Cisco Hardware and Linux Gentoo with Quagga
+Package (Zebra)": http://hosteddocs.ittoolbox.com/ke032707.pdf)
+
+#### BGP-Anycast.imn
+This is the example of BGP router configuration inserted directly in .imn file.
+```
+vi BGP-Anycast.imn
+```
+In this example BGP Anycast routing is demonstrated:
+-- IP address 8.8.8.8 is assigned to WEB1 and WEB2. 
+-- Client1 is redirected to WEB1 and Client2 is redirected to WEB2.
+-- If the link between Backbone1 and DC2 is configured to have BER=1, than after some time the traffic from Client2 is redirected to WEB1.
+
---
+++ BGP/test.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+. ../common/procedures.sh
+
+err=0
+slow=1
+
+# BGP_custom-config.imn
+eid=`imunes -b BGP-Anycast.imn | awk '/Experiment/{print $4; exit}'`
+startCheck "$eid"
+
+echo "Wait 40 sec ..."
+sleep 40
+netDump DC1@$eid eth1
+if [ $? -eq 0 ]; then
+    n=1
+    pingStatus=1
+    while [ $n -le 20 ] && [ $pingStatus -ne 0 ]; do
+        echo "Ping test $n / 20 ..."
+        pingCheck Client2@$eid 8.8.8.8 2
+        pingStatus=$?
+        n=`expr $n + 1`
+    done
+    if [ $pingStatus -eq 0 ]; then
+	echo "########## Backbone1@$eid routes"
+	himage -nt Backbone1@$eid vtysh << __END__
+	show ip route
+	exit
+__END__
+	if [ $slow -eq 1 ]; then
+	    stopNode DC2@$eid 
+	    if [ $? -eq 0 ]; then
+        echo "Wait 20 sec ..."
+		sleep 20
+
+		echo ""
+		echo "########## Backbone1@$eid routes after 45 seconds"
+		himage -nt Backbone1@$eid vtysh << __END__ 
+		show ip route
+		exit
+__END__
+
+		startNode DC2@$eid
+		if [ $? -eq 0 ]; then
+            echo "Wait 30 sec ..."
+		    sleep 30
+                    n=1
+                    pingStatus=1
+                    while [ $n -le 20 ] && [ $pingStatus -ne 0 ]; do
+                        echo "Ping test2 $n / 20 ..."
+                        pingCheck Client2@$eid 8.8.8.8 2
+                        pingStatus=$?
+                        n=`expr $n + 1`
+                    done
+                    if [ $pingStatus -eq 0 ]; then
+			sleep 4
+			readDump DC1@$eid eth1
+		    else
+			err=1
+		    fi
+		else
+		    err=1
+		fi
+	    else
+		err=1
+	    fi
+	fi
+    else
+	err=1
+    fi
+else
+    err=1
+fi
+
+readDump DC1@$eid eth1
+imunes -b -e $eid
+
+thereWereErrors $err
