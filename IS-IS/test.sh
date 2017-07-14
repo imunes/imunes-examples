93f7866eb70a3c091d094d034db232f74f1f65e6
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2017-07-14 11:46:58 +0200

    simple test; works on Linux

---
+++ IS-IS/test.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+. ../common/procedures.sh
+
+err=0
+slow=0
+
+eid=`imunes -b IS-IS.imn | awk '/Experiment/{print $4; exit}'`
+startCheck "$eid"
+
+Wait 40
+
+n=1
+pingStatus=1
+while [ $n -le 20 ] && [ $pingStatus -ne 0 ]; do
+    echo "Ping test $n / 20 ..."
+    pingCheck pc1@$eid 172.16.3.20 2
+    pingStatus=$?
+    n=`expr $n + 1`
+done
+
+if [ $pingStatus -ne 0 ]; then
+    err=1
+fi
+
+for r in R1 R2 R3
+do
+    echo "########## $r@$eid routes"
+    himage -nt $r@$eid vtysh << __END__
+    show ip route
+    exit
+__END__
+done
+
+imunes -b -e $eid
+
+thereWereErrors $err
+
