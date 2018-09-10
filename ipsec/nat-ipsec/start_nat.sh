f33e53f398ccf45f01bcfdac303e45100b3b5b70
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2018-09-10 10:27:41 +0200

    NAT startup for Linux and FreeBSD

--- ipsec/nat-ipsec/start_nat.sh
+++ ipsec/nat-ipsec/start_nat.sh
@@ -1,17 +1,22 @@
 #! /bin/sh
 
+. ../../common/procedures.sh
+
 error() {
     echo $*
     exit 2
 }
 
-kldload -n ipfw
-kldload -n ipdivert
-
 himage nat@$1 hostname \
   || error "Is simulation started? Try: Experiment->Execute"
 
-sleep 2
-himage nat@$1 natd -l -interface eth1
-hcp nat.rules nat@$1:/root
-himage nat@$1 sh /root/nat.rules
+if isOSlinux; then
+    himage nat@$eid iptables-restore < nat.rules.linux
+else
+    kldload -n ipfw
+    kldload -n ipdivert
+    sleep 2
+    himage nat@$1 natd -l -interface eth1
+    hcp nat.rules nat@$1:/root
+    himage nat@$1 sh /root/nat.rules
+fi
