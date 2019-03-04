19433ef667f0ae106555bb9303c48f2276a7d107
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2019-03-04 13:32:00 +0100

    on FreeBSD 12 prefixlen of ipv6 address obtained from dhcp is 128; router must send RAs and client must accept RAs; dhclient should use empty configuration file

--- DHCP6+RSOL/test.sh
+++ DHCP6+RSOL/test.sh
@@ -8,6 +8,30 @@ err=0
 eid=`imunes -b DHCP6.imn | awk '/Experiment/{print $4; exit}'`
 startCheck "$eid"
 
+./start_stateless $eid
+if [ $? -ne 0 ]; then
+    echo "********** START_STATELESS ERROR **********"
+    pcs=""
+    err=1
+fi
+
+for pc in $pcs; do
+    n=1
+    pingStatus=1
+    while [ $n -le 20 ] && [ $pingStatus -ne 0 ]; do
+        echo "Ping6 test $n / 20 ..."
+	ping6Check $pc@$eid fc00:2::10
+        pingStatus=$?
+        n=`expr $n + 1`
+	sleep 1
+    done
+
+    if [ $pingStatus -ne 0 ]; then
+	err=$pingStatus
+        break
+    fi
+done
+
 ./start_dhcpd6 $eid
 if [ $? -ne 0 ]; then
     echo "********** START_DHCP6 ERROR **********"
@@ -50,7 +74,7 @@ if [ $err -eq 0 ]; then
 	if isOSfreebsd; then
 	    pre="/usr/local/sbin/"
 	fi
-	himage PC3@${eid} ${pre}dhclient -6 eth0
+	himage PC3@${eid} ${pre}dhclient -6 -cf /dev/null eth0
         if [ $? -eq 0 ]; then
             sleep 2
             readDump PC3@$eid eth0
@@ -63,30 +87,6 @@ if [ $err -eq 0 ]; then
         err=1
     fi
 fi
-
-./start_stateless $eid
-if [ $? -ne 0 ]; then
-    echo "********** START_STATELESS ERROR **********"
-    pcs=""
-    err=1
-fi
-
-for pc in $pcs; do
-    n=1
-    pingStatus=1
-    while [ $n -le 20 ] && [ $pingStatus -ne 0 ]; do
-        echo "Ping6 test $n / 20 ..."
-	ping6Check $pc@$eid fc00:2::10
-        pingStatus=$?
-        n=`expr $n + 1`
-	sleep 1
-    done
-
-    if [ $pingStatus -ne 0 ]; then
-	err=$pingStatus
-        break
-    fi
-done
 
 imunes -b -e $eid
 
