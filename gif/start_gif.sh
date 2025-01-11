368f4550b29d32c0294daa7b6ba9bb71c6be4769
Denis Salopek <denisSal@users.noreply.github.com>
2025-01-11 19:08:40 +0100

    Fix gif test on FreeBSD
    Auto link-local was giving both gif interfaces the same address so no
    RIPng traffic was going.

--- gif/start_gif.sh
+++ gif/start_gif.sh
@@ -14,16 +14,20 @@ fi
 
 echo "Configuring gif tunnel on router1..."
 himage router1@$eid sysctl net.link.gif.max_nesting=2
+himage router1@$eid sysctl net.inet6.ip6.auto_linklocal=0
 gif1=`himage router1@$eid ifconfig gif create`
 himage router1@$eid ifconfig $gif1 tunnel 10.0.0.1 10.0.1.2
+himage router1@$eid ifconfig $gif1 inet6 fe80::1%$gif1 prefixlen 64
 himage router1@$eid ifconfig $gif1 inet6 fc00:1::100 fc00:3::100 prefixlen 128
-himage router1@$eid route add -inet6 fc00:4::/64 fc00:3::100
+himage router1@$eid sysctl net.inet6.ip6.auto_linklocal=1
 echo "Done."
 
 echo "Configuring gif tunnel on router2..."
 himage router2@$eid sysctl net.link.gif.max_nesting=2
+himage router2@$eid sysctl net.inet6.ip6.auto_linklocal=0
 gif2=`himage router2@$eid ifconfig gif create`
 himage router2@$eid ifconfig $gif2 tunnel 10.0.1.2 10.0.0.1
+himage router2@$eid ifconfig $gif2 inet6 fe80::2%$gif2 prefixlen 64
 himage router2@$eid ifconfig $gif2 inet6 fc00:3::100 fc00:1::100 prefixlen 128
-himage router2@$eid route add -inet6 fc00:0::/64 fc00:1::100
+himage router2@$eid sysctl net.inet6.ip6.auto_linklocal=1
 echo "Done."
